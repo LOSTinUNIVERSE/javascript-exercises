bff3981fce3ef96b2e27a5d9cbce970a5c8a4b81
abdulla <abdullaabdullaev0803@gmail.com>
2023-02-04 18:14:13 +0300

    add appropriate function to complete the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+        let sum = 0
+        if (num1 !== Number(num1) || num2 !== Number(num2)){
+            return "ERROR"
+        }
+        if (num1 < 0 || num2 < 0){
+            return "ERROR"
+        }
+        if (num1 > num2){
+            for(let i= num2 ; i <= num1; i++) {
+            sum += i }
+        } 
+        for(let i= num1 ; i <= num2; i++) {
+            sum += i }
+        return sum
+        
 };
+console.log(sumAll(10, "90"));
 
 // Do not edit below this line
 module.exports = sumAll;
