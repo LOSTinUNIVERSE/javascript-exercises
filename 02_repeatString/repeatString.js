d624b8c139ac98eef1a3ff6e473ec9e50c285b8d
abdulla <abdullaabdullaev0803@gmail.com>
2023-02-04 13:45:34 +0300

    complete exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num){
+ let store =''
+  for (let i = 0; i < num ; i++){ 
+    store += string
+  }
+  if ( num < 0 ) {
+    store = "ERROR"
+  }
+ return store
+} 
+repeatString ('', 10)
+// how do we add string to string ?
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
 
-};
+
+const removeFromArray = function(numbers, ...Arg) {
+return numbers.filter(x => !Arg.includes(x))}
+
+console.log(removeFromArray ([1, 2, 3], "1", 3)) 
 
 // Do not edit below this line
 module.exports = removeFromArray;
