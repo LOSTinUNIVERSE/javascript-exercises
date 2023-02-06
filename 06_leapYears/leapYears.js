f308b04dbdf723ac283220743e6d29a5528afcc7
abdulla <abdullaabdullaev0803@gmail.com>
2023-02-06 09:46:44 +0300

    add function that works with all conditions and passes the test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,13 +1,17 @@
-const leapYears = function(year) {
-    if(year % 4 == 0 || year % 400 ==0 && year % 100 !==0 ){
-        return true}
-    else if (year % 4 !== 0 || year % 400 !==0 && year % 100 ==0 ){
-        return false }
-        else { return false }
+const leapYears = function(year) { 
+    if (year % 4== 0 || year % 400 == 0 ){
+        if (year % 100 == 0) {
+            if ( year % 400 == 0){
+                return true
+            }
+            else {return false}
+        }
+        else {return true}
+    }
+    else {return false}
     }
     
-// console.log(leapYears(1996));
-console.log(leapYears(1900))
-
+ 
+    console.log(leapYears(700))
 // Do not edit below this line
 module.exports = leapYears;
