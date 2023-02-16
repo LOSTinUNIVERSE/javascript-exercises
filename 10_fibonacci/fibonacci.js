c6b7dfc050ed898d9d73ba24d368193f53006cc2
abdulla <abdullaabdullaev0803@gmail.com>
2023-02-16 19:28:11 +0300

    create correct function to find fibonacci number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num)
+    if (num < 1){
+        return "OOPS"
+    }
+let num1=0, num2=1, numC = num;
+for (let i =2; i <=num; i++){
+numC = num1 + num2, 
+num1 = num2,
+num2 = numC
+}      
+return numC
 };
+console.log(fibonacci("21"));
 
 // Do not edit below this line
 module.exports = fibonacci;
