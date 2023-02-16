a17ad9e75b10a0f7b6bc65347817e774c660c6b9
abdulla <abdullaabdullaev0803@gmail.com>
2023-02-16 15:06:36 +0300

    create a function to complete the test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+let regExp = /[^A-Za-z0-9]/g
+let lowRegExp = string.toLowerCase().replace(regExp,'')
 
+let reverseString = lowRegExp.split('').reverse().join('')
+return reverseString === lowRegExp
 };
+console.log(palindromes('ZZZZ car, a man, a maracaz.'));
 
 // Do not edit below this line
 module.exports = palindromes;
