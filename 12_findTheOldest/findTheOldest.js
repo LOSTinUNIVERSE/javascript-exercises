7cb4465ac131ad2dd59e95bfa0abfa5390c99b8d
abdulla <abdullaabdullaev0803@gmail.com>
2023-02-18 14:40:32 +0300

    Create a function to pass all test .

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,41 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
 
-};
+let now = new Date()
+let year = now.getFullYear()
+let sorted = {}
+  
+  const findTheOldest = function(people) {
+    for(let item of people){
+      if ('yearOfDeath' in item == false){
+         item.yearOfDeath = year 
+      };
+    }
+    people.sort((a,b)=>{
+      const lastGuy = a.yearOfDeath - a.yearOfBirth
+      const nextGuy = b.yearOfDeath - b.yearOfBirth
+      return lastGuy > nextGuy ? -1 : 1
+    })
+      sorted = people
+      return sorted[0]
+  }
+  console.log(findTheOldest(people))
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
